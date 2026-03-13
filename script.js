fc29332eafd658c8cb29f578dfb98c54db66e4ad
kushagri96 <kushagriisrich@gmail.com>
2026-03-13 18:09:34 +0530

    Update script.js

--- script.js
+++ script.js
@@ -88,3 +88,31 @@ function animate(){
 }
 
 animate();
+
+
+/* NAVBAR ACTIVE LINK */
+const sections = document.querySelectorAll("section, .hero");
+const navLinks = document.querySelectorAll(".nav-links a");
+
+window.addEventListener("scroll", () => {
+
+  let current = "";
+
+  sections.forEach(section => {
+    const sectionTop = section.offsetTop - 120;
+    const sectionHeight = section.offsetHeight;
+
+    if(pageYOffset >= sectionTop){
+      current = section.getAttribute("id");
+    }
+  });
+
+  navLinks.forEach(link => {
+    link.classList.remove("active");
+
+    if(link.getAttribute("href") === "#" + current){
+      link.classList.add("active");
+    }
+  });
+
+});
